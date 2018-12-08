57a1d7261dc86b6eff353b3d5d174caef215689b
yuliBao <1624689815@qq.com>
2018-12-08 18:27:20 +0800

    组织了一下结构 添加了一点注释

--- server.cpp
+++ server.cpp
@@ -16,7 +16,8 @@
 
 #pragma comment (lib,"Ws2_32.lib")
 
-void GetTimeFS(char *output, int buffer_size, const char * format)
+
+void GetTimeStamp(char *output, const char * format)
 {
 	time_t now_time;
 	struct tm info;
@@ -24,8 +25,9 @@ void GetTimeFS(char *output, int buffer_size, const char * format)
 	time(&now_time);
 	localtime_s(&info, &now_time);
 
-	strftime(output, buffer_size-1, format, &info);
+	strftime(output, LOG_T_MAXLEN - 1, format, &info);
 }
+
 
 void LoadSocket(int major_version, int minor_version)
 {
@@ -42,213 +44,201 @@ void LoadSocket(int major_version, int minor_version)
 }
 
 
+SmtpServer& operator<<(SmtpServer& server, char *data_send)
+{
+	//发送数据
+	send(server.session_socket_, data_send, strlen(data_send), 0);
+
+	//记录日志，输出到标准输出
+	GetTimeStamp(server.log_time_buffer_, LOG_T_F);
+	server.log_file_ << server.log_time_buffer_ << "reply:  " << data_send;
+	std::cout << "reply:  " << data_send;
+
+	return server;
+}
+
+
+int operator>>(SmtpServer& server, char *data_receive)
+{
+	//接收数据，如果没有数据则阻塞
+	int data_len = 0;
+	data_len = recv(server.session_socket_, data_receive, server.buffer_size_, NULL);
+	GetTimeStamp(server.log_time_buffer_, LOG_T_F);
+
+	//记录日志，输出到标准输出
+	data_receive[data_len] = '\0';
+	server.log_file_ << server.log_time_buffer_ << "receive:  " << data_receive;
+	std::cout << "receive:  " << data_receive;
+	
+	return data_len;
+}
+
+
 SmtpServer::SmtpServer(int buffer_size) :listen_socket_(INVALID_SOCKET), buffer_size_(buffer_size), buffer_(NULL)
 {
-	//Get log file's name
+	//通过样式 LOG_FN_F 获取LOG文件名
 	char log_fn[30];
-	GetTimeFS(log_fn, sizeof(log_fn), LOG_FN_F);
+	GetTimeStamp(log_fn, LOG_FN_F);
 
-	//Open log file
+	//打开Log文件
 	log_file_.open(log_fn);
 
-	//Initialize buffer
-	buffer_ = new char[buffer_size_];
-	if (buffer_ == NULL)
-	{
-		std::cout << "failed to new" << std::endl;
-		WSACleanup();
-		exit(4);
-	}
-
-	//Create SOCKET
+	//获取socket 地址族ipv4 流式SOCKET 协议TCP
 	listen_socket_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (listen_socket_ == INVALID_SOCKET)
 	{
-		std::cout << "socket failed with error: " << WSAGetLastError() << std::endl;
+		GetTimeStamp(log_time_buffer_, LOG_T_F);
+		log_file_ << log_time_buffer_ << "socket failed with error : "
+			<< WSAGetLastError() << std::endl;
+
 		WSACleanup();
 		exit(2);
 	}
 
+	//申请缓冲内存
+	buffer_ = new char[buffer_size_];
+	if (buffer_ == NULL)
+	{
+		GetTimeStamp(log_time_buffer_, LOG_T_F);
+		log_file_ << log_time_buffer_ << "failed to new a " << buffer_
+			<< "bytes buffer" << std::endl;
+
+		WSACleanup();
+		exit(4);
+	}
+
 }
+
 
 void SmtpServer::Listen(unsigned short listen_port)
 {
+	//设置地址和端口
 	listen_addr_ = "127.0.0.1";
 	listen_port_ = listen_port;
 
-	//Initialize address and port
+	//服务器端口地址初始化
 	sockaddr_in sin;
 	memset(&sin, 0, sizeof(sin));
-
+	
+	//地址族 ipv4 
 	sin.sin_family = AF_INET;
 	sin.sin_port = htons(listen_port_);
 	sin.sin_addr.S_un.S_addr = inet_addr(listen_addr_);
 
 
-	//Bind address and port
+	//绑定端口和地址
 	if (bind(listen_socket_, (LPSOCKADDR)&sin, sizeof(sin)) == SOCKET_ERROR)
 	{
-		std::cout << "bind failed with error: " << WSAGetLastError() << std::endl;
+		GetTimeStamp(log_time_buffer_, LOG_T_F);
+		log_file_ << log_time_buffer_ << "bind failed with error: "
+			<< WSAGetLastError() << std::endl;
+
 		closesocket(listen_socket_);
 		WSACleanup();
 		exit(3);
 	}
 
-	//start to listen
+	//开始监听端口
 	if (listen(listen_socket_, SOMAXCONN) == SOCKET_ERROR)
 	{
-		std::cout << "listen failed with error: " << WSAGetLastError() << std::endl;
+		GetTimeStamp(log_time_buffer_, LOG_T_F);
+		log_file_ << log_time_buffer_ << "listen failed with error: "
+			<< WSAGetLastError() << std::endl;
+
 		closesocket(listen_socket_);
 		WSACleanup();
 		exit(3);
 	}
+
+	GetTimeStamp(log_time_buffer_, LOG_T_F);
+	log_file_<< log_time_buffer_<< "Server listenning on " << inet_ntoa(sin.sin_addr)
+		<< ":" << ntohs(sin.sin_port) << "......" << std::endl;
+
 	std::cout << "Server listenning on " << inet_ntoa(sin.sin_addr)
 		<< ":" << ntohs(sin.sin_port) << "......" << std::endl;
 
 }
 
+
 void SmtpServer::Start()
 {
-
-	SOCKET session_socket = INVALID_SOCKET;
+	//客户端地址初始化
 	sockaddr_in host_addr;
 	int host_addr_len = sizeof(host_addr);
 	memset(&host_addr, 0, host_addr_len);
 
 	while (1)
 	{
-		session_socket = accept(listen_socket_, (SOCKADDR*)&host_addr, &host_addr_len);
-		if (session_socket == INVALID_SOCKET)
+		session_socket_ = INVALID_SOCKET;
+		//接收连接，如果没有连接则挂起
+		session_socket_ = accept(listen_socket_, (SOCKADDR*)&host_addr, &host_addr_len);
+		GetTimeStamp(log_time_buffer_, LOG_T_F);
+
+		if (session_socket_ == INVALID_SOCKET)
 		{
-			std::cout << "accept failed with error: " << WSAGetLastError() << std::endl;
+			log_file_ << GetTimeStamp << "accept failed with error: "
+				<< WSAGetLastError() << std::endl;
+
 			closesocket(listen_socket_);
 			WSACleanup();
 			exit(5);
 		}
-		std::cout << "accepted a connection from " << inet_ntoa(host_addr.sin_addr)
-			<< ":" << ntohs(host_addr.sin_port) << std::endl;
-		GetTimeFS(log_time_buffer_, sizeof(log_time_buffer_), LOG_T_F);
+
 		log_file_ << log_time_buffer_ << "accepted a connection from " << inet_ntoa(host_addr.sin_addr)
 			<< ":" << ntohs(host_addr.sin_port) << std::endl;
+		std::cout << "accepted a connection from " << inet_ntoa(host_addr.sin_addr)
+			<< ":" << ntohs(host_addr.sin_port) << std::endl;
 
+		//建立连接 服务器首先回复220
+		send(session_socket_, RB220, sizeof(RB220), NULL);
+
+		GetTimeStamp(log_time_buffer_, LOG_T_F);
+		log_file_ << log_time_buffer_ << RB220 << std::endl;
 		std::cout << "reply:  " << RB220 << std::endl;
-		log_file_ << log_time_buffer_ << "reply:  " << RB220 << std::endl;
-		send(session_socket, RB220, strlen(RB220), 0);
-
-		HandleCmd(session_socket);
 	}
 }
 
-int SmtpServer::HandleCmd(SOCKET session_socket)
+
+void SmtpServer::SaveMailData()
 {
-	int data_len;
-	const char *pr = NULL;
+	int data_len = 0;
+	int data_count = 0;
+	data_file_.open(".\\Data\\mail_data.txt", std::ios::ate);
 
-	//EHLO ----  250 OK
-	data_len = recv(session_socket, buffer_, buffer_size_, 0);
-	buffer_[data_len] = '\0';
-	log_file_ << log_time_buffer_ << buffer_;
-	std::cout << "receive:  " << buffer_;
-
-	pr = RB250;
-	log_file_ << log_time_buffer_ << pr;
-	std::cout << "reply:  " << pr;
-	send(session_socket, pr, strlen(pr), 0);
-
-	//MAIL FROM ------250 OK
-	data_len = recv(session_socket, buffer_, buffer_size_, 0);
-	buffer_[data_len] = '\0';
-	log_file_ << log_time_buffer_ << buffer_;
-	std::cout << "receive:  " << buffer_;
-
-	pr = RB250;
-	log_file_ << log_time_buffer_ << pr;
-	std::cout << "reply:  " << pr;
-	send(session_socket, pr, strlen(pr), 0);
-
-	//RCPT TO -----250 OK
-	data_len = recv(session_socket, buffer_, buffer_size_, 0);
-	buffer_[data_len] = '\0';
-	log_file_ << log_time_buffer_ << buffer_;
-	std::cout << "receive:  " << buffer_;
-
-	pr = RB250;
-	log_file_ << log_time_buffer_ << pr;
-	std::cout << "reply:  " << pr;
-	send(session_socket, pr, strlen(pr), 0);
-
-	//DATA ----- 354 End data with <CR><LF>.<CR><LF>
-	data_len = recv(session_socket, buffer_, buffer_size_, 0);
-	buffer_[data_len] = '\0';
-	log_file_ << log_time_buffer_ << buffer_;
-	std::cout << "receive:  " << buffer_;
-
-	pr = RB354;
-	log_file_ << log_time_buffer_ << pr;
-	std::cout << "reply:  " << pr;
-	send(session_socket, pr, strlen(pr), 0);
-
-	//Receive data and Wati for <CR><LF>.<CR><LF> ---------- 250 OK
 	while (true)
 	{
-		std::cout << "receiving data........." << std::endl;
-		data_len = recv(session_socket, buffer_, buffer_size_, 0);
+		data_len = recv(session_socket_, buffer_, buffer_size_, 0);
+		data_count += data_len;
 		buffer_[data_len] = '\0';
 
-		//Check End Point And Break;
-		if (strcmp(CHECK_END_POINT(buffer_, data_len), END_OF_DATA) == 0)
+		GetTimeStamp(log_time_buffer_, LOG_T_F);
+		log_file_ << log_time_buffer_ << "receiving data........." << data_count<<std::endl;
+		std::cout << "receiving data........." << std::endl;
+
+
+		//检查数据结束标志
+		if (strcmp(CHECK_DATA_END(buffer_, data_len), END_OF_DATA) == 0)
 		{
+			data_file_ << buffer_;
 			std::cout << "receive:  " << buffer_;
+
+			GetTimeStamp(log_time_buffer_, LOG_T_F);
+			log_file_ << log_time_buffer_ << "finished" << std::endl;
+
 			std::cout << "finished" << std::endl;
 			break;
 		}
-
+		data_file_ << buffer_;
 		std::cout << "receive:  " << buffer_;
 	}
-
-	pr = RB250;
-	log_file_ << log_time_buffer_ << pr;
-	std::cout << "reply:  " << pr;
-	send(session_socket, pr, strlen(pr), 0);
-
-	//QUIT ----- 221 Bye
-	data_len = recv(session_socket, buffer_, buffer_size_, 0);
-	buffer_[data_len] = '\0';
-	log_file_ << log_time_buffer_ << buffer_;
-	std::cout << "receive:  " << buffer_;
-
-	pr = RB221;
-	log_file_ << log_time_buffer_ << pr;
-	std::cout << "reply:  " << pr;
-	send(session_socket, pr, strlen(pr), 0);
-	return 0;
 }
+
 
 SmtpServer::~SmtpServer()
 {
 	delete[]buffer_;
 	log_file_.close();
+	data_file_.close();
 	closesocket(listen_socket_);
 	WSACleanup();
 }
-
-/*
-SmtpClient::SmtpClient(int buffer_size) :remote_socket_(INVALID_SOCKET), buffer_size_(buffer_size), buffer_(NULL)
-{
-	//Initialize buffer
-	buffer_ = new char[buffer_size_];
-	if (buffer_ == NULL)
-	{
-		std::cout << "failed to new" << std::endl;
-		WSACleanup();
-		exit(4);
-	}
-
-	remote_socket_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (remote_socket_ == INVALID_SOCKET)
-	{
-		std::cout << "socket failed with error: " << WSAGetLastError() << std::endl;
-		WSACleanup();
-		exit(2);
-	}
-}*/
--- smtp.h
+++ smtp.h
@@ -1,73 +1,125 @@
 #pragma once
 
+#ifndef _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#endif
+
+#ifndef _WINSOCK2API_
+#include <WinSock2.h>
+#endif
+
 #ifndef _SMTP_
-#define SMTP
+#define  SMTP
 #endif
 
 #ifndef _FSTREAM_
 #include <fstream>
 #endif
 
-#ifndef _WINSOCK_DEPRECATED_NO_WARNINGS
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-#endif
-
+/*
+* LOG_FN_F 定义了服务器Log文件的目录以及命名样式
+* LOG_T_F  定义了服务器Log文件中时间戳的样式
+* LOG_T_MAXLEN 定义了LOG_FN_F 与 LOG_T_F 样式对应的最大长度
+*/
 #define LOG_FN_F ".\\Log\\Log-%Y%m%d%H%M%S.txt"
 #define LOG_T_F "%m-%d-%H:%M:%S ----- "
+#define LOG_T_MAXLEN 30
 
+/*
+* RB[三位回复码] 定义了服务器的响应列表
+*/
 #define RB220 "220 localhost\r\n"
 #define RB250 "250 OK\r\n"
 #define RB354 "354 End data with <CR><LF>.<CR><LF>\r\n"
 #define RB221 "221 Bye\r\n"
 
-#define RB334_USER "334 dXNlcm5hbWU6"
-#define RB334_PWD "334 UGFzc3dvcmQ6"
-
-#define CHECK_END_POINT(buffer,data_len) (buffer+data_len-5)
+/*
+* 定义客户端的的命令列表
+*/
+#define EHLO "EHLO "
+#define MF "MAIL FROM: "
+#define RT "RCPT TO: "
+#define DATA "DATA"
+#define QT "QUIT"
 #define END_OF_DATA "\r\n.\r\n"
 
-#include<WinSock2.h>
+/*
+* 宏函数 CHECK_DATA_END 返回 邮件数据结束检测点的地址
+* buffer：指向缓冲的指针
+* data_len：数据块的长度
+* cmd 需要检测的命令
+*/
+#define CHECK_DATA_END(buffer, data_len) (buffer+data_len-strlen(END_OF_DATA))
 
-void GetTimeFS(char *output_time, int buffer_size, const char *format);
+
+/*
+* 宏函数 GET_PARA 返回对应命令的参数地址
+* buffer：指向缓冲的指针
+* cmd：需要获得参数的命令
+*/
+#define GET_PARA(buffer, cmd) (buffer+strlen(cmd))
+
+
+/********
+* 获取指定格式的时间戳
+* char *output_time 输出参数：带回指定格式的字符串形式时间戳
+* const char *format
+********/
+void GetTimeStamp(char *output_time, const char *format);
 
 void LoadSocket(int major_version, int minor_version);
 
 class SmtpServer
 {
 private:
-	SOCKET listen_socket_;
+	/*服务器地址、端口、会话套接字、服务器监听套接字*/
 	const char *listen_addr_;
 	unsigned short listen_port_;
+	SOCKET listen_socket_;
+	SOCKET session_socket_;
 
-	int buffer_size_;
+	/*服务器接收缓冲*/
 	char* buffer_;
+	int buffer_size_;
 
+	/*服务器日志 邮件数据文件 时间戳缓冲*/
 	std::ofstream log_file_;
-	char log_time_buffer_[30];
+	std::ofstream data_file_;
+	char log_time_buffer_[LOG_T_MAXLEN];
 
-public:
-	void Listen(unsigned short listen_port);
-	void Start();
 private:
-	int HandleCmd(SOCKET session_socket);
-	/*void HandleData(SOCKET session_socket)*/
+	int HandleMailData(SOCKET session_socket);
 public:
+	/***********
+	*构造函数打开Log文件、初始化服务器监听套接字、申请服务器缓冲
+	*析构函数释放SOCKET资源、释放服务器缓冲、关闭邮件数据文件、关闭Log文件
+	***********/
 	SmtpServer(int buffer_size);
 	~SmtpServer();
+
+	/***********
+	 *Listen 传入监听端口 绑定地址(默认127.0.0.1)和端口并开始监听
+	 *Start  启动服务器并开始接收连接 
+	***********/
+	void Listen(unsigned short listen_port);
+	void Start();
+
+	/*该函数在回调函数中调用，在收到DATA命令后，储存邮件至文件中*/
+	void SaveMailData();
+
+	/***********
+     * SmtpServer类重载了 << 和 >>两个运算符，重新定义了两个运算符的行为
+	*
+	* SmtpServer& operator<<(SmtpServer&, char *send);
+	* 向远程已经连接的客户端发送数据
+	*
+	* void operator>>(SmtpServer&, char *receive);
+	* 从远程已经链接的客户端接收数据, 存储在缓冲区receive中，返回接收的数据量
+	* receive的大小 统一定位Server类缓冲成员 buffer_size_，一般情况下，receive直接用类成员buffer_
+	*
+	* << 和 >> 会在标准输出和Log文件中同时记录输入/输入内容
+	***********/
+	friend SmtpServer& operator<<(SmtpServer& server, char *data_send);
+	friend int operator>>(SmtpServer& server, char *data_receive);
+public:
 };
-
-/*class SmtpClient
-{
-private:
-	SOCKET remote_socket_;
-	struct sockaddr_in remote_addr_;
-
-	int buffer_size_;
-	char* buffer_;
-
-public:
-	void Connect(struct sockaddr_in remote_addr_);
-public:
-	SmtpClient(int buffer_size);
-	~SmtpClient();
-};*/
